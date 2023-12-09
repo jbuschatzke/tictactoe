ae3f12ebb1ecda2d5564736884165b0415271aeb
jbuschatzke <joezzles@gmail.com>
2023-12-08 21:56:18 -0800

    Rework code to remove Cell() function from array and implement functional win condition

--- script.js
+++ script.js
@@ -1,89 +1,108 @@
-// The gameboard represents the state of the board where
-// each square holds a Cell
 function Gameboard() {
     const rows = 3;
     const columns = 3;
     board = [];
-//Create a 2d array that will represent the state of the game using a nested loop
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
-        }
+    
+    for (let i=0; i<rows;i++) {
+      board[i] = [];
+      for (let j=0; j<columns; j++){
+        board[i].push(0);
+      };
     };
-//getBoard will enable UI rendering    
+    
     const getBoard = () => board;
-
+    const printBoard = () => console.log(board);
+    
     const playMarker = (row, column, player) => {
-        if (board[row][column].getValue() == 0) {
-            board[row][column].addMarker(player);
-        } else {
-            return;
-        }
+      tryAgain = false;
+      if (board[row][column] == 0){
+        board[row][column] = player;
+      } else {
+        tryAgain = true;
+      };
     };
 
-    const printBoard = () => {
-      const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
-      console.log(boardWithCellValues);
-    };
+    return { playMarker, getBoard, printBoard }
+  };
 
-    return { getBoard, playMarker, printBoard };
-}
-
-//Cell represents one square on the board and will have a value of
-//O: empty
-//1: Player One's marker
-//2: Player Two's marker
-function Cell() {
-    let value = 0;
-    const addMarker = (player) => {
-        value = player;
-    };
-
-    const getValue = () => value;
-
-    return{addMarker, getValue};
-};
-
-function GameController(
+  
+  
+  function GameController(
     playerOneName = "Player One",
     playerTwoName = "Player Two"
-){
-    const board = Gameboard();
-
+  ){
+    const gameBoard = Gameboard();
+    const board = gameBoard.getBoard();
+    
     const players = [
-        {
-            name: playerOneName,
-            marker: 1
-        },
-        {
-            name: playerTwoName,
-            marker: 2
-        }
+      {
+        name: playerOneName,
+        marker: 1
+      },
+      {
+        name: playerTwoName,
+        marker: 2
+      }
     ];
-
+    
     let activePlayer = players[0];
-
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    
+    const switchPlayerTurn = () =>  {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
     const getActivePlayer = () => activePlayer;
-
+    
     const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`)
+      gameBoard.printBoard();
+      console.log(`${getActivePlayer().name}'s turn.`)
     };
+
+    function isGameOver() {
+        // check upper row
+        if (board[0][1] == board[0][0] && board[0][1] == board[0][2] && board[0][1] != 0) {
+          return board[0][1]
+        }
+        // check lower row
+        if (board[2][1] == board[2][0] && board[2][1] == board[2][2] && board[2][1] != 0) {
+          return board[2][1]
+        }
+        // check left column
+        if (board[1][0] == board[0][0] && board[1][0] == board[2][0] && board[1][0] != 0) {
+          return board[1][0]
+        }
+        // check right column
+        if (board[1][2] == board[0][2] && board[1][2] == board[2][2] && board[1][2] != 0) {
+          return board[1][2]
+        }
+        // check center row, column, and diagonals
+        if (
+          board[1][1] != 0 &&
+          ((board[1][1] == board[1][0] && board[1][1] == board[1][2]) ||
+            (board[1][1] == board[0][1] && board[1][1] == board[2][1]) ||
+            (board[1][1] == board[0][0] && board[1][1] == board[2][2]) ||
+            (board[1][1] == board[2][0] && board[1][1] == board[0][2]))
+        ) {
+          return board[1][1]
+        }
+      
+        return 0
+      };
 
     const playRound = (row, column) => {
-        board.playMarker(row, column, getActivePlayer().marker);
-
+      gameBoard.playMarker(row, column, getActivePlayer().marker);
+      const checkGame = isGameOver();
+      if (tryAgain == true){
+        console.log("Try again");
+      } else if (checkGame != 0) {
+        console.log(`${getActivePlayer().name} wins!`)
+      } else {
         switchPlayerTurn();
         printNewRound();
+      };
     };
-
     printNewRound();
-
+    
     return { playRound, getActivePlayer }
-};
-
-const game = GameController();
+  };
+  
+  const game = GameController();
